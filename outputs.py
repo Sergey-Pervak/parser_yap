f4519d57076bd48b745256e2c35f338486713568
Sergey Pervak <pervak102kaf@mail.ru>
2025-03-14 10:26:45 +0300

    Учебный парсер почти готов

--- outputs.py
+++ outputs.py
@@ -1,13 +1,23 @@
 # outputs.py
+import csv
+import datetime as dt
+import logging
+
 from prettytable import PrettyTable
+
+from constants import BASE_DIR, DATETIME_FORMAT
 
 
 def control_output(results, cli_args):
-    if cli_args.pretty:
+    output = cli_args.output
+    if output == 'pretty':
         # Вывод в формате PrettyTable.
         pretty_output(results)
+    elif output == 'file':
+        # Вывод данных в файл csv.
+        file_output(results, cli_args)
     else:
-        # Вывод по умолчанию.
+        # Вывод по умолчанию — в терминал построчно.
         default_output(results)
 
 
@@ -28,3 +38,29 @@ def pretty_output(results):
     table.add_rows(results[1:])
     # Печатаем таблицу.
     print(table)
+
+
+def file_output(results, cli_args):
+    results_dir = BASE_DIR / 'results'
+    results_dir.mkdir(exist_ok=True)
+    # Получаем режим работы парсера из аргументов командной строки.
+    parser_mode = cli_args.mode
+    # Получаем текущие дату и время.
+    now = dt.datetime.now()
+    # Сохраняем текущие дату-время в указанном формате.
+    # Результат будет выглядеть вот так: 2021-06-18_07-40-41.
+    now_formatted = now.strftime(DATETIME_FORMAT)
+    # Собираем имя файла из полученных переменных:
+    # «режим работы программы» + «дата и время записи» + формат (.csv).
+    file_name = f'{parser_mode}_{now_formatted}.csv'
+    # Получаем абсолютный путь к файлу с результатами.
+    file_path = results_dir / file_name
+    # Через контекстный менеджер открываем файл по сформированному ранее пути
+    # в режиме записи 'w', в нужной кодировке utf-8.
+    with open(file_path, 'w', encoding='utf-8') as f:
+        # Создаём «объект записи» writer.
+        writer = csv.writer(f, dialect='unix')
+        # Передаём в метод writerows список с результатами парсинга.
+        writer.writerows(results)
+
+    logging.info(f'Файл с результатами был сохранён: {file_path}')
